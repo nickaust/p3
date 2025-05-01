d871cf58dd11be4942aefb73c086b9e427744fb4
Nick Austin <nickaustin27@icloud.com>
2025-05-01 12:16:17 -0700

    First commit

---
+++ lab-04.js
@@ -0,0 +1,26 @@
+// Include express module, and create an instance of express
+const express = require('express');
+const app = express();
+
+// Set listen IP and port
+const listenPort = 8080;
+const listenIP = '127.0.0.1';
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// GET /
+app.get('/', (req, res) => {
+  res.status(200).type('text/plain; charset=utf-8');
+  res.send('<h1>Hello from Express GET / route!</h1>');
+});
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).send('404 Not Found');
+});
+
+// Start the server
+app.listen(listenPort, listenIP, () => {
+  console.log(`Server running at http://${listenIP}:${listenPort}`);
+});
